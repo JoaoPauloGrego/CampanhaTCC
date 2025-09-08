564f9a136d21c655f524ce25f4c9d855a6506347
MuriloFerreir4 <muriloferreira.silva09@gmail.com>
2025-09-08 16:20:39 -0300

    estrutura-BD

--- db/database.js
+++ db/database.js
@@ -1,31 +1,39 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/campanha.db');
+const db2 = new sqlite3.Database('./db/TCC.db')
 
-db.serialize(() => {
-  // Tabela de Tipos de Roupa
-  db.run(`CREATE TABLE IF NOT EXISTS roupas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    tipo TEXT NOT NULL,
-    pontuacao INTEGER NOT NULL
+db2.serialize(() => {
+  // Tabela de Campanhas
+  db2.run(`CREATE TABLE IF NOT EXISTS CAMPANHAS (
+    id_campanha INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_campanha TEXT NOT NULL,
+    id_turmas INTEGER,
+    status_sala INTEGER NOT NULL,
+    dt_inicial TEXT NOT NULL,
+    dt_final TEXT NOT NULL,
+    pt_total-sala INTEGER NOT NULL,
+    FOREIGN KEY(id_turmas) REFERENCES TURMAS(id_turmas)
   )`);
 
   // Tabela de Turmas
-  db.run(`CREATE TABLE IF NOT EXISTS turmas (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+  db2.run(`CREATE TABLE IF NOT EXISTS TURMAS (
+    id_turmas INTEGER PRIMARY KEY AUTOINCREMENT,
     turma TEXT NOT NULL,
-    docente TEXT NOT NULL
+    docente TEXT NOT NULL,
+    d_total INTEGER NOT NULL,
+    d_atual INTEGER NOT NULL,
+    status INTEGER NOT NULL,
+    FOREIGN KEY(id_turmas) REFERENCES CAMPANHAS(id_turmas)
   )`);
-
-  // Tabela de Doações
-  db.run(`CREATE TABLE IF NOT EXISTS doacoes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    turma_id INTEGER NOT NULL,
-    roupa_id INTEGER NOT NULL,
-    quantidade INTEGER NOT NULL,
-    data TEXT NOT NULL,
-    FOREIGN KEY(turma_id) REFERENCES turmas(id),
-    FOREIGN KEY(roupa_id) REFERENCES roupas(id)
+  
+  // Tabela de Tipos de Itens
+  db2.run(`CREATE TABLE IF NOT EXISTS ITENS (
+    id_item INTEGER PRIMARY KEY AUTOINCREMENT,
+    itens TEXT NOT NULL,
+    ponto INTEGER NOT NULL,
+    id_campanha INTEGER,
+    FOREIGN KEY(id_campanha) REFERENCES CAMPANHAS(id_campanha)
   )`);
 });
 
-module.exports = db;
+module.exports = db2;
